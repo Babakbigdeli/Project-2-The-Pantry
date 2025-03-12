2a8a88e4ecb297e56230eb094abdcebc1a4e3667
Babak <babakbigdeli@gmail.com>
2025-03-12 14:33:49 +0000

    Update database config to use JawsDB

--- config/db_config.js
+++ config/db_config.js
@@ -14,13 +14,18 @@ function getDbConfig(env) {
       "host": "127.0.0.1",
       "dialect": "mysql"
     },
-    "production": {
-      "username": "ndp3jxz9p476q85t",
-      "password": "a319ypo5kr83o3x8",
-      "database": "esbvka1fdiaxi7s2",
-      "host": "lmc8ixkebgaq22lo.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
-      "dialect": "mysql"
-    }
+    "production": process.env.JAWSDB_URL
+      ? {
+          "use_env_variable": "JAWSDB_URL",
+          "dialect": "mysql"
+        }
+      : {
+          "username": "ndp3jxz9p476q85t",
+          "password": "a319ypo5kr83o3x8",
+          "database": "esbvka1fdiaxi7s2",
+          "host": "lmc8ixkebgaq22lo.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
+          "dialect": "mysql"
+        }
   };
 
   return config[env];
