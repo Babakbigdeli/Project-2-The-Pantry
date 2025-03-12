47e236a6a6165cfd37156b7f7a5925ef3887d2f3
Babak <babakbigdeli@gmail.com>
2025-03-12 14:40:58 +0000

    Fix database connection for production

--- config/db_config.js
+++ config/db_config.js
@@ -1,34 +1,40 @@
 function getDbConfig(env) {
+  if (env === "production") {
+    const mysql = require("mysql2");
+    const dbUrl = process.env.JAWSDB_URL; // Get DB URL from Heroku
+    const config = new URL(dbUrl);
+
+    return {
+      username: config.username,
+      password: config.password,
+      database: config.pathname.substring(1), // Remove leading '/'
+      host: config.hostname,
+      dialect: "mysql"
+    };
+  }
+
+  // Default config for development and test
   const config = {
-    "development": {
-      "username": "root",
-      "password": "Catherine62#",
-      "database": "pantryDB",
-      "host": "127.0.0.1",
-      "dialect": "mysql"
+    development: {
+      username: "root",
+      password: "Catherine62#",
+      database: "pantryDB",
+      host: "127.0.0.1",
+      dialect: "mysql",
     },
-    "test": {
-      "username": "root",
-      "password": "Catherine62#",
-      "database": "pantryDB",
-      "host": "127.0.0.1",
-      "dialect": "mysql"
+    test: {
+      username: "root",
+      password: "Catherine62#",
+      database: "pantryDB",
+      host: "127.0.0.1",
+      dialect: "mysql",
     },
-    "production": process.env.JAWSDB_URL
-      ? {
-          "use_env_variable": "JAWSDB_URL",
-          "dialect": "mysql"
-        }
-      : {
-          "username": "ndp3jxz9p476q85t",
-          "password": "a319ypo5kr83o3x8",
-          "database": "esbvka1fdiaxi7s2",
-          "host": "lmc8ixkebgaq22lo.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
-          "dialect": "mysql"
-        }
   };
 
   return config[env];
 }
+
+module.exports = getDbConfig;
+
 
 module.exports = getDbConfig;
